bf875768dfe9000bb9d66d3acddc64aa9762d3bd
rassk09 <rassk09@gmail.com>
2018-11-07 09:15:52 +0300

    INIT

---
+++ src/Providers/AdminServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Rassk09\Admin\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AdminServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
